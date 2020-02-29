7c952cfc79baeea6a9d4384d88c4f04bc05a5030
Yunus Emre MELEKİS <44137415+yunusemremeleks@users.noreply.github.com>
2020-03-01 00:03:13 +0300

    Add files via upload

---
+++ 6.hafta/DC_motor_hiz_kontrolu.ino
@@ -0,0 +1,31 @@
+int motor_i=5;
+int motor_g=4;
+int hiz=3;
+void setup() {
+pinMode(motor_i,OUTPUT);
+pinMode(motor_g,OUTPUT);
+pinMode(hiz,OUTPUT);
+}
+void loop() {
+  digitalWrite(motor_i,HIGH);
+  digitalWrite(motor_g,LOW); 
+  for (int i=0;i<255;i++){
+  analogWrite(hiz,i);
+  delay(20);
+  }
+  for (int i=255;i>0;i--){
+  analogWrite(hiz,i);
+  delay(20);
+  }
+  digitalWrite(motor_i,LOW);
+  digitalWrite(motor_g,HIGH);
+   
+  for (int i=0;i<255;i++){
+  analogWrite(hiz,i);
+  delay(20);
+  }
+  for (int i=255;i>0;i--){
+  analogWrite(hiz,i);
+  delay(20);
+  }
+}
